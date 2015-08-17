b086734b0e97e4c0a52f368001c37057e3efec3a
Miles Hill <mile1618@gmail.com>
2015-08-16 19:04:24 -0500

    Converted to initialization cells

--- SchoolDisplay/SchoolDisplay.m
+++ SchoolDisplay/SchoolDisplay.m
@@ -1,12 +1,11 @@
 (* ::Package:: *)
 
-(* ::Input:: *)
-(*(* Output for easy column display *)*)
-(*SetOptions[DisplayColumn,{Alignment->Left,Dividers->All,Spacings->1.5,Background->White}];*)
-(*DisplayColumn[title_,{headers__},{sols__}]/;(Length[{headers}]==Length[{sols}]):=Block[{Title},*)
-(*(* customization for row headers *)*)
-(*Title[size_:16]:=Style[#,{FontFamily->"Calibri",FontSize->size,Background->Lighter[Gray, 0.5]}]&;*)
-(**)
-(*Column[*)
-(*Riffle[Title[]/@{headers},{sols}],Options[DisplayColumn]]//Labeled[#,Title[20][title],Top]&//Panel*)
-(*];*)
+(* Output for easy column display *)
+SetOptions[DisplayColumn,{Alignment->Left,Dividers->All,Spacings->1.5,Background->White}];
+DisplayColumn[title_,{headers__},{sols__}]/;(Length[{headers}]==Length[{sols}]):=Block[{Title},
+(* customization for row headers *)
+Title[size_:16]:=Style[#,{FontFamily->"Calibri",FontSize->size,Background->Lighter[Gray, 0.5]}]&;
+
+Column[
+Riffle[Title[]/@{headers},{sols}],Options[DisplayColumn]]//Labeled[#,Title[20][title],Top]&//Panel
+];
